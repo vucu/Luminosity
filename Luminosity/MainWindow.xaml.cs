ca25a88e13751a65b773331b1b2e06421712bbe9
Astrid <astrid@gmail.com>
2020-05-05 15:26:08 -0700

    add image from clipboard

---
+++ Luminosity/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Luminosity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPasteFromClipboard(object sender, RoutedEventArgs e)
+        {
+            MyImage.Source = ImageFromClipboardDib();
+        }
+
+        private ImageSource ImageFromClipboardDib()
+        {
+            MemoryStream ms = Clipboard.GetData("DeviceIndependentBitmap") as MemoryStream;
+            if (ms != null)
+            {
+                byte[] dibBuffer = new byte[ms.Length];
+                ms.Read(dibBuffer, 0, dibBuffer.Length);
+
+                BITMAPINFOHEADER infoHeader =
+                    BinaryStructConverter.FromByteArray<BITMAPINFOHEADER>(dibBuffer);
+
+                int fileHeaderSize = Marshal.SizeOf(typeof(BITMAPFILEHEADER));
+                int infoHeaderSize = infoHeader.biSize;
+                int fileSize = fileHeaderSize + infoHeader.biSize + infoHeader.biSizeImage;
+
+                BITMAPFILEHEADER fileHeader = new BITMAPFILEHEADER();
+                fileHeader.bfType = BITMAPFILEHEADER.BM;
+                fileHeader.bfSize = fileSize;
+                fileHeader.bfReserved1 = 0;
+                fileHeader.bfReserved2 = 0;
+                fileHeader.bfOffBits = fileHeaderSize + infoHeaderSize + infoHeader.biClrUsed * 4;
+
+                byte[] fileHeaderBytes =
+                    BinaryStructConverter.ToByteArray<BITMAPFILEHEADER>(fileHeader);
+
+                MemoryStream msBitmap = new MemoryStream();
+                msBitmap.Write(fileHeaderBytes, 0, fileHeaderSize);
+                msBitmap.Write(dibBuffer, 0, dibBuffer.Length);
+                msBitmap.Seek(0, SeekOrigin.Begin);
+
+                return BitmapFrame.Create(msBitmap);
+            }
+            return null;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 2)]
+        private struct BITMAPFILEHEADER
+        {
+            public static readonly short BM = 0x4d42; // BM
+
+            public short bfType;
+            public int bfSize;
+            public short bfReserved1;
+            public short bfReserved2;
+            public int bfOffBits;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct BITMAPINFOHEADER
+        {
+            public int biSize;
+            public int biWidth;
+            public int biHeight;
+            public short biPlanes;
+            public short biBitCount;
+            public int biCompression;
+            public int biSizeImage;
+            public int biXPelsPerMeter;
+            public int biYPelsPerMeter;
+            public int biClrUsed;
+            public int biClrImportant;
+        }
+    }
+}
